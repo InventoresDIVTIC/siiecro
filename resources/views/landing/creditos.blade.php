0d6868287e393c98b0495407beba74ac937acf68
Esteban Bocardo Medel <toti_irre@hotmail.com>
2021-10-07 19:51:23 -0500

    PAgina de creditos

---
+++ resources/views/landing/creditos.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.landing')
+
+@section('body')
+
+    <section class="section about">
+        <div class="container">
+            <div class="row justify-content-center mt-4">
+                <div class="col-12 text-center">
+                    <h3>Integrantes del proyecto.</h3>
+                </div>
+            </div>
+            <div class="divider mx-auto my-4"></div>
+            <div class="row">
+                <div class="col-12">
+                    <h4>Escuela de Conservación y Restauración de Occidente</h4>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <p>
+                        <i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;Diana Quintero Gonzalez <br>
+                        <i><a href="mailto:d d.quintero@ecro.edu.mx"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;d d.quintero@ecro.edu.mx</a></i>
+                    </p>
+                    <p>
+                        <i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;Geovanna M. Ochoa Manzo <br>
+                        <i><a href="mailto:g.ochoa@ecro.edu.mx"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;g.ochoa@ecro.edu.mx</a></i>
+                    </p>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <strong>Actualización/Alimentación de base de datos</strong>
+                    <br>
+                    <br>
+                    <p>
+                        <i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;Gabriela Contreras Fierros <br>
+                    </p>
+                    <p>
+                        <i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;Noemí Monserrat Calderón Chavarrín <br>
+                    </p>
+                    <p>
+                        <i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;Bernardo Orduño Guerra <br>
+                    </p>
+                    <p>
+                        <i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;Sarah Flores Rodríguez <br>
+                    </p>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-12">
+                    <h4>Universidad de Guadalajara</h4>
+                    <p>
+                        <i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;José Luis David Bonilla Carranza <br>
+                        <i><a href="mailto:jose.bcarranza@academicos.udg.mx"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;jose.bcarranza@academicos.udg.mx</a></i>
+                    </p>
+                </div>
+
+                <div class="col-sm-12 col-md-6">
+                    <strong>Versión Alfa</strong>
+                    <br>
+                    <br>
+                    <p>
+                        <i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;Irving Elías Flores Bravo <br>
+                        <i><a href="mailto:irving_yo14@hotmail.com"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;irving_yo14@hotmail.com</a></i>
+                    </p>
+                    <p>
+                        <i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;Héctor Joel Cholico Chávez <br>
+                        <i><a href="mailto:cholyko@hotmail.com"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;cholyko@hotmail.com</a></i>
+                    </p>
+                    <p>
+                        <i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;García Ríos Tonatiuh <br>
+                        <i><a href="mailto:tonatiuhgarciarios@hotmail.com"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;tonatiuhgarciarios@hotmail.com</a></i>
+                    </p>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <strong>Versión Beta</strong>
+                    <br>
+                    <br>
+                    <p>
+                        <i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;Esteban Bocardo Medel <br>
+                        <i><a href="mailto:estebane03597@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;estebane03597@gmail.com</a></i>
+                    </p>
+                    <p>
+                        <i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;Luis Alberto García Rodríguez <br>
+                        <i><a href="mailto:luis.garcia7630@alumnmos.udg.mx"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;luis.garcia7630@alumnmos.udg.mx</a></i>
+                    </p>
+                    <p>
+                        <i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;Kevin Arnold Sahagun Jauregui <br>
+                        <i><a href="mailto:kevin.sahagun@alumnos.udg.mx"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;kevin.sahagun@alumnos.udg.mx</a></i>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('scripts')
+
+@endsection
